b483b3dbb4d3fc741f5b630aa83caa0a9765dc6e
TomLEPERT <hijikatame16@gmail.com>
2026-01-07 10:26:24 +0100

    quete_2

---
+++ backend/search/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('search/', include('search.urls')),
+]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
