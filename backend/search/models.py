31b0eb45dd80ec81bc78eda3914116c2cc1c038e
TomLEPERT <hijikatame16@gmail.com>
2026-01-07 12:02:26 +0100

    quetes_4

--- backend/search/models.py
+++ backend/search/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Produit(models.Model):
+    ingredient = models.TextField()
+    
+    def __str__(self):
+        return self.ingredient
