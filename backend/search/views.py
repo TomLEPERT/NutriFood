e1c8cee1aff553f74e4b2f9a4eafd67f6d141661
TomLEPERT <hijikatame16@gmail.com>
2026-01-26 15:30:32 +0100

    quetes_6

--- backend/search/views.py
+++ backend/search/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import Produit
 
 def index(request):
-    return HttpResponse("Hey ! tu es sur l'index de l'application search du projet NutriFood.")
+    return HttpResponse("Home OK. Teste /<id>/ (ex: /1/)")
+
+def produit_detail(request, code):
+    produit = get_object_or_404(Produit, pk=code)
+    return render(request, "search/produit_detail.html", {"produit": produit})
